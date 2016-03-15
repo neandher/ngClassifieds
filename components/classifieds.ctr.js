546c5b5f2feced0167911f221364d4e8ae19d620
Neandher Carlos <neandher89@gmail.com>
2016-03-15 16:57:52 -0300

    Aula - 005 Filtering Data - Finalizada

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,12 +4,93 @@
 
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function ($scope, classifiedsFactory) {
+        .controller("classifiedsCtrl", function ($scope, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
             classifiedsFactory.getClassifieds().then(function (classifieds) {
-
                 $scope.classifieds = classifieds.data;
+                $scope.categories = getCategories($scope.classifieds);
             });
+
+            var contact = {
+                name: "Ryan Chenkie",
+                phone: "(555) 555-5555",
+                email: "ryanchenkie@gmail.com"
+            }
+
+            $scope.openSideBar = function () {
+                $mdSidenav("left").open();
+            };
+
+            $scope.closeSideBar = function () {
+                $mdSidenav("left").close();
+            };
+
+            $scope.saveClassified = function (classified) {
+
+                if (classified) {
+
+                    classified.contact = contact;
+
+                    $scope.classifieds.push(classified);
+                    $scope.classified = {};
+                    $scope.closeSideBar();
+                    showToast("Classified Saved!");
+                }
+            };
+
+            $scope.editClassified = function (classified) {
+
+                $scope.editing = true;
+                $scope.openSideBar();
+                $scope.classified = classified;
+
+            };
+
+            $scope.saveEdit = function () {
+                $scope.editing = false;
+                $scope.classified = {};
+                $scope.closeSideBar();
+                showToast("Edit Saved!");
+            };
+
+            $scope.deleteClassified = function (event, classified) {
+
+                var confirm = $mdDialog.confirm()
+                    .title("Are you sure you want to delete " + classified.title + '?')
+                    .ok('Yes')
+                    .cancel('No')
+                    .targetEvent(event);
+
+                $mdDialog.show(confirm).then(function () {
+                    var index = $scope.classifieds.indexOf(classified)
+                    $scope.classifieds.splice(index, 1);
+                }, function () {
+                    //...
+                });
+            }
+
+            function showToast(message) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content(message)
+                        .position("top, right")
+                        .hideDelay(3000)
+                );
+            }
+
+            function getCategories(classifieds) {
+
+                var categories = [];
+
+                angular.forEach(classifieds, function (item) {
+                    angular.forEach(item.categories, function (category) {
+                        categories.push(category);
+                    });
+                })
+
+                return _.uniq(categories);
+            }
+
         });
 
 })();
